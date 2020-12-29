aa0079ccf270d500a9077b562d0cae6645b431fc
Sandeep Chenna kumar <sandeepkchenna@gmail.com>
2020-12-29 22:43:58 +0530

    added cors

--- react-form-server/app.js
+++ react-form-server/app.js
@@ -2,6 +2,9 @@ const server = require("express");
 const app = server();
 const port = process.env.PORT || 3001;
 const loginRouter = require("./routes/route.login.js");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(server.json());
 app.use("/api", loginRouter);
--- react-forms/src/App.js
+++ react-forms/src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { email: "", password: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      let response = await axios.post("http://localhost:3001/api/login", {
+        name: this.state.name,
+        password: this.state.password,
+      });
+      console.log(response);
+    } catch (error) {
+      throw error;
+    }
+  };
+  render() {
+    return (
+      <div className="d-flex justify-content-center h-100">
+        <div className="card">
+          <div className="card-header">
+            <h3>Sign In</h3>
+            <div className="d-flex justify-content-end social_icon">
+              <span>
+                <i className="fab fa-facebook-square"></i>
+              </span>
+              <span>
+                <i className="fab fa-google-plus-square"></i>
+              </span>
+              <span>
+                <i className="fab fa-twitter-square"></i>
+              </span>
+            </div>
+          </div>
+          <div className="card-body">
+            <form>
+              <div className="input-group form-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    <i className="fas fa-user"></i>
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control"
+                  placeholder="username"
+                  onChange={this.handleChange}
+                  value={this.state.name}
+                />
+              </div>
+              <div className="input-group form-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">
+                    <i className="fas fa-key"></i>
+                  </span>
+                </div>
+                <input
+                  type="password"
+                  name="password"
+                  className="form-control"
+                  placeholder="password"
+                  onChange={this.handleChange}
+                  value={this.state.password}
+                />
+              </div>
+              <div className="row align-items-center remember">
+                <input type="checkbox" />
+                Remember Me
+              </div>
+              <div className="form-group">
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn float-right login_btn"
+                  onClick={this.handleSubmit}
+                />
+              </div>
+            </form>
+          </div>
+          <div className="card-footer">
+            <div className="d-flex justify-content-center links">
+              Don't have an account?<a href="#">Sign Up</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
