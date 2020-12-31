99e7eecb9aeb19114b21bb89f2d2546177c47852
Sandeep Chenna kumar <sandeepkchenna@gmail.com>
2020-12-31 08:16:39 +0530

    Search State Updated

--- react-app-github/src/components/users/Search.js
+++ react-app-github/src/components/users/Search.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { SearchText: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+  }
   render() {
     return (
       <div>
         <form className="form">
-          <input type="text" name="text" placeholder="Search Users..." />
+          <input
+            type="text"
+            name="SearchText"
+            placeholder="Search Users..."
+            value={this.state.SearchText}
+            onChange={this.handleChange}
+          />
           <input
             type="submit"
-            value="Search"
+            onClick={this.handleSubmit}
             className="btn btn-dark btn-block"
           />
         </form>
