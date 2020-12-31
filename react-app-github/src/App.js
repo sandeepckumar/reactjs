bd56785e1a5142cf81d4e17731d732a021bf1a34
Sandeep Chenna kumar <sandeepkchenna@gmail.com>
2020-12-31 07:02:56 +0530

    Calling API and passing it as props for users

--- react-app-github/src/App.js
+++ react-app-github/src/App.js
@@ -2,14 +2,29 @@ import "./App.css";
 import React, { Component } from "react";
 import { Navbar } from "./components/layouts/";
 import { Users } from "./components/users";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    try {
+      this.setState({ loading: true });
+      const response = await axios.get("https://api.github.com/users");
+      this.setState({ loading: false, users: response.data });
+      console.log(response.data);
+    } catch (error) {
+      throw error;
+    }
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
