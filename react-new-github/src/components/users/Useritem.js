a60543b9699382ef03ff8b0f49fb277d4ffa0cea
Sandeep Chenna kumar <sandeepkchenna@gmail.com>
2021-01-12 07:45:54 +0530

    userItem to functional component

--- react-new-github/src/components/users/Useritem.js
+++ react-new-github/src/components/users/Useritem.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
 
-class UserItem extends Component {
-  render() {
-    const { html_url, avatar_url, login } = this.props;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "120px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            Github Profile
-          </a>
-        </div>
+const UserItem = ({ avatar_url, html_url, login }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "120px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          Github Profile
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
