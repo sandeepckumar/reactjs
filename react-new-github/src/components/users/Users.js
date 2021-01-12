b2c63bb55ce34d1d0374bc27f31935e73fed1762
Sandeep Chenna kumar <sandeepkchenna@gmail.com>
2021-01-12 08:40:50 +0530

    Changed users component to functional

--- react-new-github/src/components/users/Users.js
+++ react-new-github/src/components/users/Users.js
@@ -1,33 +1,37 @@
 import UserItem from "./Useritem";
-import React, { Component } from "react";
+import React from "react";
 import { Spinner } from "../layouts";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  render() {
-    const userStyle = {
-      display: "grid",
-      gridTemplateColumns: "repeat(3,1fr)",
-      gridGap: "1rem",
-    };
-    return (
-      <>
-        {this.props.loading ? (
-          <Spinner />
-        ) : (
-          <div style={userStyle}>
-            {this.props.users.map((user) => (
-              <UserItem
-                login={user.login}
-                html_url={user.html_url}
-                avatar_url={user.avatar_url}
-                key={user.id}
-              />
-            ))}
-          </div>
-        )}
-      </>
-    );
-  }
-}
+const Users = (props) => {
+  const userStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(3,1fr)",
+    gridGap: "1rem",
+  };
+  return (
+    <>
+      {props.loading ? (
+        <Spinner />
+      ) : (
+        <div style={userStyle}>
+          {props.users.map((user) => (
+            <UserItem
+              login={user.login}
+              html_url={user.html_url}
+              avatar_url={user.avatar_url}
+              key={user.id}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+UserItem.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  users: PropTypes.array.isRequired,
+};
 
 export default Users;
