f55a14a06c8ce715b7193906074e7af64fc22365
Sandeep Chenna kumar <sandeepkchenna@gmail.com>
2021-01-12 08:56:29 +0530

    added changeHandler to search

--- react-new-github/src/components/users/Search.js
+++ react-new-github/src/components/users/Search.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  changeHandler = (e) => {
+    this.setState({ text: e.target.value });
+  };
+
   render() {
     return (
       <div>
         <form className="form">
-          <input type="text" name="text" placeholder="Search Users" />
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users"
+            value={this.state.text}
+            onChange={this.changeHandler}
+          />
           <input
             type="submit"
             value="Search"
